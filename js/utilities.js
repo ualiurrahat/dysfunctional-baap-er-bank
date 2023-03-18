8789aefe35c750d309ce5d02e43e195c003a338e
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-18 20:24:28 +0600

    fixed bug with withdrawal issue

--- js/utilities.js
+++ js/utilities.js
@@ -2,8 +2,9 @@ function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
   const inputFieldValue = parseFloat(inputFieldValueString);
-  return inputFieldValue;
   inputField.value = "";
+  return inputFieldValue;
+
 }
 
 function getTextElementValueById(elementId) {
